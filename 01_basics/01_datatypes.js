47d60c864606276bb77f310ecd3502dfacf6e18a
Ajit saha <109420507+AjitSaha@users.noreply.github.com>
2025-01-25 19:01:48 +0000

    practice datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,17 @@
+const array=["ajit","siraj"]
+console.log(array);
+console.log(typeof array);
+let ajit={
+    name:"ajit",
+    age:21
+}
+console.log(ajit)
+console.log(typeof ajit);
+let saha=function(){
+    console.log("siraj")
+}
+console.log(saha);
+console.log(typeof saha);
+
+
+
---
+++ 01_basics/01_variable.js
@@ -0,0 +1,9 @@
+const name1="ajit";
+let p_no=7978850353;
+var loc="Karnataka";
+let branch;
+
+console.log(name1,p_no,loc);
+
+console.table([name1,p_no,loc,branch])
+
